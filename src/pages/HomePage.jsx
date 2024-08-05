013ed5a7206c8715ca3aeec7ffe6424e49f9df01
Usuario <stefan.migo@gmail.com>
2024-08-04 23:42:19 -0400

    header logic

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,13 +7,11 @@ import FeaturesMobile from "../components/FeaturesMobile";
 import AboutUs from "../components/AboutUs";
 import Blog from "../components/Blog";
 import Contact from "../components/Contact";
-import HeaderIndex from '../components/HeaderIndex';
 import GalleryHomePage from "../components/GalleryHomePage";
 
 const HomePage = () => {
   return (
     <div className="lg:pt-[5.25rem]">
-      <HeaderIndex />
       <Hero />
       <Features />
       <AboutUs />
